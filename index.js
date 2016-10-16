fe7c75b8df74518e70febeeb94d7d365f052d6d8
Martin <martinbutts@clear.net.nz>
2016-10-16 18:22:50 +1300

    add spans variable

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ function startTimer(id, deadline){
                     + '<span>' + timer.minutes + '</span>'
                     + '<span>' + timer.seconds + '</span>';
 
+    var spans = clock.getElementsByTagName("span");
+    animateClock(spans[3]);
+    if(timer.seconds == 59) animateClock(spans[2]);
+    if(timer.minutes == 59 && timer.seconds == 59) animateClock(spans[1]);
+    if(timer.hours == 23 && timer.minutes == 59 && timer.seconds == 59) animateClock(spans[0]);
+
+
     if(timer.total < 1){
       clearInterval(timerInterval);
       clock.innerHTML = '<span>0</span><span>0</span><span>0</span><span>0</span>';
