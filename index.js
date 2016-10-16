03643deddd480e9290dc6589ef46d434f0ef7d9c
Martin <martinbutts@clear.net.nz>
2016-10-16 15:16:10 +1300

    add startTimer function

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+function startTimer(id, deadline){
+  var timerInterval = setInterval(function(){
+    var clock = document.getElementById(id);
+    var timer = updateTimer(deadline);
+
+    clock.innerHTML = '<span>' + timer.days + '</span>'
+                    + '<span>' + timer.hours + '</span>'
+                    + '<span>' + timer.minutes + '</span>'
+                    + '<span>' + timer.seconds + '</span>';
+  }, 1000);
+}
+
+
 window.onload = function(){
   var deadline = new Date("January 1, 2045 00:00:01");
   startTimer("clock", deadline);
