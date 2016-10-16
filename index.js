f21f25c0d79cb00bcc287a32fe7a3f5c7b199644
Martin <martinbutts@clear.net.nz>
2016-10-17 11:17:48 +1300

    add gitignore

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ function updateTimer(deadline){
   };
 }
 
+
+function animateClock(span){
+  span.className = "turn";
+  setTimeout(function(){
+    span.className = "";
+  },700);
+}
+
 function startTimer(id, deadline){
   var timerInterval = setInterval(function(){
     var clock = document.getElementById(id);
@@ -19,23 +27,25 @@ function startTimer(id, deadline){
                     + '<span>' + timer.minutes + '</span>'
                     + '<span>' + timer.seconds + '</span>';
 
+    //animations
     var spans = clock.getElementsByTagName("span");
     animateClock(spans[3]);
     if(timer.seconds == 59) animateClock(spans[2]);
     if(timer.minutes == 59 && timer.seconds == 59) animateClock(spans[1]);
     if(timer.hours == 23 && timer.minutes == 59 && timer.seconds == 59) animateClock(spans[0]);
 
-
+    //check for end of timer
     if(timer.total < 1){
       clearInterval(timerInterval);
       clock.innerHTML = '<span>0</span><span>0</span><span>0</span><span>0</span>';
     }
+
 
   }, 1000);
 }
 
 
 window.onload = function(){
-  var deadline = new Date("January 1, 2045 00:00:01");
+  var deadline = new Date("April 25, 2016 17:15:00");
   startTimer("clock", deadline);
 };
